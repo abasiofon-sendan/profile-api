d69359b336aaa42e3e3ab08db42ad79301633418
abasiofon-sendan <abasiofon135@gmail.com>
2025-10-18 13:11:36 +0100

    allowed host to the domain

--- CatfactProfile/settings.py
+++ CatfactProfile/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0kg)o&h@!h0ylz9lb%#tb4p33x450kd^bk!vjgkiht*&76^_i8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
